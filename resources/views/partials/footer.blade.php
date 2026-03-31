22b8697b7bacfaf3252d825d0dc44731103bf4e8
gennaroderosa10 <gennaroderosa5@gmail.com>
2026-03-31 16:55:23 +0200

    creata bozza struttura header e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-secondary">
+    <div class="container">
+        <div class="row justify-content-center text-white">
+<h3>footer</h3>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="container ">
+        <div class="row justify-content-center">
+           
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+             <div class="col-lg-4">
+                <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+            </div>
+  
+  <div class="collapse navbar-collapse col-lg-8" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">CHARACTERS</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">COMICS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MOVIES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">SHOP</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+    </div>
+    
+</header>
